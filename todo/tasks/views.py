7106b424b5987557c56f310e41dd7f4b566f2f90
Kathara Sasikumar <katharasasikumar007@gmail.com>
2024-07-01 23:21:57 +0530

    update index view to display tasks

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+from .models import *
+from .forms import *
+
 def index(request):
-    return render(request, 'tasks/list.html')    
+    tasks = Task.objects.all()
+
+	context = {'tasks':tasks, 'form':form}
+    return render(request, 'tasks/list.html', context)    
