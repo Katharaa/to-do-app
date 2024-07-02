1644bc4e826899311806d9a1f116be9d74f3aea8
Kathara Sasikumar <katharasasikumar007@gmail.com>
2024-07-02 22:00:38 +0530

    Adjusted spacing in views.py for consistent formatting

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,24 +1,31 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
 from .models import *
 from .forms import *
 
+# Create your views here.
+
 def index(request):
-    tasks = Task.objects.all()
-    form = TaskForm()
+	tasks = Task.objects.all()
 
-    if request.method == 'POST':
-        form = TaskForm(request.POST)
-        if form.is_valid():
-            form.save()
-        return redirect('/')
+	form = TaskForm()
 
-    context = {'tasks': tasks, 'form': form}
-    return render(request, 'tasks/list.html', context)
+	if request.method =='POST':
+		form = TaskForm(request.POST)
+		if form.is_valid():
+			form.save()
+		return redirect('/')
+
+
+	context = {'tasks':tasks, 'form':form}
+	return render(request, 'tasks/list.html', context)
 
 def updateTask(request, pk):
-    task = Task.objects.get(id=pk)
-    form = TaskForm(instance=task)
+	task = Task.objects.get(id=pk)
+
+	form = TaskForm(instance=task)
+
 
     if request.method == 'POST':
         form = TaskForm(request.POST, instance=task)
