eed7d3688b04f372c3bf19afdc7d618faed7f1bf
Kathara Sasikumar <katharasasikumar007@gmail.com>
2024-07-02 21:56:32 +0530

    Removed outdated URL pattern in tasks/urls.py

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -2,7 +2,4 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="list"),
-    path('update_task/<str:pk>/', views.updateTask, name="update_task"),
-
 ]
