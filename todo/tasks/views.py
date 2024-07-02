b2c67fb5e1bc1676ecf8d64fca74fa551e2c794e
Kathara Sasikumar <katharasasikumar007@gmail.com>
2024-07-02 19:12:01 +0530

    Refactor index view to include task form rendering

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
-
 from .models import *
 from .forms import *
 
 def index(request):
     tasks = Task.objects.all()
-
-	context = {'tasks':tasks, 'form':form}
-    return render(request, 'tasks/list.html', context)    
+    form = TaskForm()
+    context = {'tasks': tasks, 'form': form}
+    return render(request, 'tasks/list.html', context)
